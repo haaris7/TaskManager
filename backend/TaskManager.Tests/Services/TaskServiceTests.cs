6b3ec9f8428c7d3509a5be37dab0270c2f1ccb5a
haaris7 <haarisimtiaz@gmail.com>
2025-10-08 17:15:17 +0200

    Setting up unit tests for taskservice
    I have tested the endpoints using Swagger, however I will create some unit tests for the service layer before merging for good measure

---
+++ backend/TaskManager.Tests/Services/TaskServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using TaskManager.Application.Services;
+using TaskManager.Application.Interfaces;
+
+namespace TaskManager.Tests.Services;
+
+public class TaskServiceTests
+{
+    private readonly Mock<ITaskRepository> _mockTaskRepository;
+    private readonly Mock<IUserRepository> _mockUserRepository;
+    private readonly TaskService _taskService;
+
+    public TaskServiceTests()
+    {
+        _mockTaskRepository = new Mock<ITaskRepository>();
+        _mockUserRepository = new Mock<IUserRepository>();
+
+        // create the service we want to test using our mock dependencies
+        _taskService = new TaskService(_mockTaskRepository.Object, _mockUserRepository.Object);
+    }
+}
